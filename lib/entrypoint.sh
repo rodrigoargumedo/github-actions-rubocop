f3cb89290a0eb09d3abbbf3ad7139747f051452c
Rodrigo Argumedo <7613139+rodrigoargumedo@users.noreply.github.com>
2023-01-04 12:13:44 -0600

    Update entrypoint.sh

--- lib/entrypoint.sh
+++ lib/entrypoint.sh
@@ -2,9 +2,9 @@
 
 set -e
 
-gem install rubocop -v 0.80.1
-gem install rubocop-rspec -v 1.38.1
-gem install rubocop-performance -v 1.5.2
-gem install rubocop-rails -v 2.4.2
+gem install rubocop
+gem install rubocop-rspec
+gem install rubocop-performance
+gem install rubocop-rails
 
 ruby /action/lib/index.rb
